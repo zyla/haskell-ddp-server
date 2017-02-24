510bd093c13c35833347057b9eea565fc7527856
Maciej Bielecki <zyla@prati.pl>
2017-02-24 12:36:26 +0100

    Handle app state

--- src/Network/DDP/Server.hs
+++ src/Network/DDP/Server.hs
@@ -17,27 +17,32 @@ data DataEvent =
   | E_Changed Changed
   | E_Removed Removed
 
-data Config = Config
-  { callMethod :: Connection -> Text -> [Value] -> IO (Either Error Value)
-  , subscribe :: Connection -> Text -> [Value] -> (DataEvent -> IO ()) -> IO Unsubscribe
+data Config s = Config
+  { connect :: ConnectionInfo -> IO s
+  , callMethod :: s -> Text -> [Value] -> IO (Either Error Value)
+  , subscribe :: s -> Text -> [Value] -> (DataEvent -> IO ()) -> IO Unsubscribe
   }
 
-data Server = Server { server_config :: Config }
+data Server s = Server { server_config :: Config s }
 
-newServer :: Config -> IO Server
+newServer :: Config s -> IO (Server s)
 newServer = return . Server
 
-data Connection = Connection
-  { connection_config :: Config
-  , connection_state :: TVar ConnectionState
+data ConnectionInfo = ConnectionInfo
+
+data Connection s = Connection
+  { connection_config :: Config s
+  , connection_state :: TVar (ConnectionState s)
   , connection_sendMessage :: ServerMessage -> IO ()
   }
 
-data ConnectionState =
+data ConnectionState s =
     New
-  | Connected { subs :: TVar (HM.HashMap SubId Unsubscribe) }
+  | Connected
+      { subs :: TVar (HM.HashMap SubId Unsubscribe)
+      , appState :: s }
 
-newConnection :: Server -> (ServerMessage -> IO ()) ->  IO Connection
+newConnection :: Server s -> (ServerMessage -> IO ()) ->  IO (Connection s)
 newConnection server sendMessage  = do
   state <- newTVarIO New
   return Connection
@@ -49,8 +54,8 @@ newConnection server sendMessage  = do
 pass :: Applicative f => f ()
 pass = pure ()
 
-processMessage :: Connection -> ClientMessage -> IO ()
-processMessage conn@(Connection Config{callMethod,subscribe} stateVar _) msg = do
+processMessage :: Connection s -> ClientMessage -> IO ()
+processMessage (Connection Config{callMethod,subscribe,connect} stateVar sendMessage) msg = do
   -- FIXME: RACE CONDITION: Two threads may C_Connect at the same time.
   state <- atomically $ readTVar stateVar
   case state of
@@ -58,21 +63,22 @@ processMessage conn@(Connection Config{callMethod,subscribe} stateVar _) msg = d
       C_Connect{} -> do
         -- TODO check versions
         subsVar <- newTVarIO HM.empty
-        sendMessage conn $ S_Connected $ Protocol.Connected $ SessionId ""
-        atomically $ writeTVar stateVar $ Connected subsVar
+        appState <- connect ConnectionInfo
+        sendMessage $ S_Connected $ Protocol.Connected $ SessionId ""
+        atomically $ writeTVar stateVar $ Connected subsVar appState
 
       _ ->
         -- TODO: return "not connected" error
         pass
 
-    Connected subsVar -> case msg of
+    Connected subsVar appState -> case msg of
 
       C_Connect{} ->
         -- TODO error: already connected
         pass
 
       C_Ping (Ping id) ->
-        sendMessage conn (S_Pong (Pong id))
+        sendMessage (S_Pong (Pong id))
 
       C_Pong {} ->
         -- Timeouts should be handled during receive,
@@ -80,16 +86,16 @@ processMessage conn@(Connection Config{callMethod,subscribe} stateVar _) msg = d
         pass
 
       C_Sub Sub{sub_id,sub_name,sub_params} -> do
-        let onEvent event = sendMessage conn (fromDataEvent event)
+        let onEvent event = sendMessage (fromDataEvent event)
             fromDataEvent = \case
               E_Added   val -> S_Added val
               E_Changed val -> S_Changed val
               E_Removed val -> S_Removed val
 
-        unsub <- subscribe conn sub_name sub_params onEvent
+        unsub <- subscribe appState sub_name sub_params onEvent
         -- FIXME doesn't handle duplicate subId case
         atomically $ modifyTVar subsVar $ HM.insert sub_id unsub
-        sendMessage conn $ S_Ready $ Ready [sub_id]
+        sendMessage $ S_Ready $ Ready [sub_id]
 
       C_Unsub Unsub{unsub_id} -> do
 
@@ -109,14 +115,14 @@ processMessage conn@(Connection Config{callMethod,subscribe} stateVar _) msg = d
             unsubscribe
 
       C_Method Method{method_method,method_params,method_id} -> do
-        result <- callMethod conn method_method method_params
-        sendMessage conn $ S_Result $ Result method_id result
+        result <- callMethod appState method_method method_params
+        sendMessage $ S_Result $ Result method_id result
 
-sendMessage :: Connection -> ServerMessage -> IO ()
-sendMessage = connection_sendMessage
-
-debugConfig = Config callMethod subscribe
+debugConfig = Config connect callMethod subscribe
   where
+
+    connect _ = putStrLn "connect"
+
     callMethod _ method args = do
       putStrLn $ "callMethod " ++ show method ++ " " ++ show args
       return (Right (object []))
